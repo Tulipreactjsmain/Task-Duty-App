25d3b02edcc63e06cc3be0bc4c42e2b062c9ce12
Tulipreactjsmain <rdhnft@gmail.com>
2023-09-26 10:30:55 +0100

    Created task controllers

---
+++ server/models/auth.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: [true, "Email already exist"],
+  },
+  password: {
+    type: String,
+  },
+  profileImg: {
+    type: String,
+  },
+},
+{
+  timestamps: true,  
+}
+);
+
+const User = mongoose.models.User || mongoose.model('User', userSchema)
+export default User
