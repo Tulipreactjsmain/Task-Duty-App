a82606921e990a343fa2a5eb1da0bff6c4553d35
Tulipreactjsmain <rdhnft@gmail.com>
2023-10-03 12:06:38 +0100

    Home MobileView

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,23 +5,24 @@ export default function Navbar() {
   return (
     <>
       <div
-        className="navDiv d-flex align-items-center justify-content-between px-5 border-bottom"
+        className="navDiv d-flex align-items-center justify-content-between px-3 px-large px-md-5 border-bottom"
         style={{ height: "93px" }}
       >
         <NavLink
           to="/"
-          className="d-flex gap-2 justify-content-center align-items-center"
+          className="d-flex gap-2 justify-content-center align-items-center "
         >
-          <Image src="https://res.cloudinary.com/techbro/image/upload/v1696279498/Task%20Duty/logo_pshjts.png" />{" "}
+          <Image style={{height:"41px",}} src="https://res.cloudinary.com/techbro/image/upload/v1696322350/Task%20Duty/Group_1_l4ydlp.png" />
           <span
-            style={{ fontWeight: "600", fontSize: "27.37px", color: "#2D0050" }}
+          className="logoName d-none d-lg-block"
+            style={{ fontWeight: "600", color: "#2D0050" }}
           >
             TaskDuty
           </span>
         </NavLink>
         <div className="d-flex gap-4 justify-content-center align-items-center">
-          <NavLink className="fw-500 fs-5 text-black">New Task</NavLink>
-          <NavLink className="fw-500 fs-5 text-black">All Tasks</NavLink>
+          <NavLink className="fw-500 fs-5 text-black d-none d-lg-block">New Task</NavLink>
+          <NavLink className="fw-500 fs-5 text-black  d-none d-lg-block">All Tasks</NavLink>
           <NavLink>
             <i className="bi bi-person-circle fs-3 text-black"></i>
           </NavLink>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,9 +4,11 @@ import Carousel from "react-bootstrap/Carousel";
 export default function Home() {
   return (
     <>
-      <Row className="pt-5">
-        <div className="d-flex justify-content-between px-5 align-items-center">
-          <Col style={{ maxWidth: "535px" }}>
+      <Row className="d-flex justify-content-between px-large px-md-5 px-3 gap-5 align-items-center pt-5">
+        {/* <div className="d-flex justify-content-between px-5 align-items-center">
+          
+        </div> */}
+        <Col  xs={12} lg={6} style={{ maxWidth: "535px" }}>
             <h1>
               Manage your Tasks On{" "}
               <span style={{ color: "#974FD0" }}>TaskDuty</span>
@@ -19,29 +21,28 @@ export default function Home() {
             </p>
             <Button className="border-0" style={{ backgroundColor: "#974FD0" }}>Go to My Tasks</Button>
           </Col>
-          <Col className="heroImg">
-            <Carousel fade controls={false} indicators={false}>
-              <Carousel.Item>
+          <Col   xs={12} lg={6} className="heroImg">
+            <Carousel className="custom-carousel" fade controls={false} indicators={false}>
+              <Carousel.Item className="carousel-inner">
                 <Image
-                  className="heroImg"
-                  src="https://res.cloudinary.com/techbro/image/upload/v1696279497/Task%20Duty/Property_1_Frame_1_mpj6yo.png"
+                  className="w-100"
+                  src="https://res.cloudinary.com/techbro/image/upload/v1696322351/Task%20Duty/Property_1_Frame_1_atulpd.png"
                 />
               </Carousel.Item>
               <Carousel.Item>
                 <Image
-                  className="heroImg"
-                  src="https://res.cloudinary.com/techbro/image/upload/v1696279497/Task%20Duty/Property_1_Frame_2_sjkgcw.png"
+                  className="w-100"
+                  src="https://res.cloudinary.com/techbro/image/upload/v1696322352/Task%20Duty/Property_1_Frame_3_e1c69l.png"
                 />
               </Carousel.Item>
               <Carousel.Item>
                 <Image
-                  className="heroImg"
-                  src="https://res.cloudinary.com/techbro/image/upload/v1696279498/Task%20Duty/Property_1_Frame_3_qyxbrb.png"
+                  className="w-100"
+                  src="https://res.cloudinary.com/techbro/image/upload/v1696322351/Task%20Duty/Property_1_Frame_1_atulpd.png"
                 />
               </Carousel.Item>
             </Carousel>
           </Col>
-        </div>
       </Row>
     </>
   );
